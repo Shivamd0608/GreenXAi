8f02459b3657acb8a5d5c127402e92902894941a
PAVANTEJ-05 <nptpavantej79@gmail.com>
2026-01-12 22:19:52 +0530

    components added

---
+++ frontend/src/components/layout.js
@@ -0,0 +1,27 @@
+// app/layout.js - CLEAN VERSION
+import './globals.css';
+import { Web3Provider } from '../contexts/Web3Context';
+import LayoutWrapper from '../components/LayoutWrapper';
+import Navigation from "../components/ui/Navigation"
+
+export const metadata = {
+  title: 'GreenXchange - Carbon Credit Trading',
+  description: 'Modern platform for carbon credit trading',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/* NO bg class here */}
+      <body className="antialiased">
+       
+        <Web3Provider>
+          <LayoutWrapper> 
+            <Navigation/>
+            {children}
+          </LayoutWrapper>
+       </Web3Provider>
+      </body>
+    </html>
+  );
+}
